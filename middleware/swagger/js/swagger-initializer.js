692854b24eb4883970ae0ca6217a88383f92e012
linx <sulinke1133@gmail.com>
2024-12-15 19:27:49 +0800

    update: bump swagger-ui to v5.18.2

--- middleware/swagger/js/swagger-initializer.js
+++ middleware/swagger/js/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-
+  console.log('swagger ui v5.18.2')
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "openapi.yaml",
